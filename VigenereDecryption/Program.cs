e05dbb92588815eb53f06666be1a93de217323c7
wited <wited@LAPTOP-CO67K0CT>
2024-08-26 11:32:12 +0300

    Proje dosyası ekle.

---
+++ VigenereDecryption/Program.cs
@@ -0,0 +1,18 @@
+﻿char[] alphabet = "ABCÇDEFGĞHIİJKLMNOÖPRSŞTUÜVYZ".ToCharArray();   //Enter letters of the alphabet in this field
+char[] cipherText = "ŞifreliMetin".ToUpper().ToCharArray();   //Enter plain text in this field
+char[] key = "Anahtar".ToUpper().ToCharArray();   //Enter the key in this field
+
+int[] keyNums = new int[key.Length];
+for (int i = 0; i < keyNums.Length; i++)
+{
+    keyNums[i] = Array.IndexOf(alphabet, key[i]);
+}
+
+string plainText = string.Empty;
+for (int i = 0; i < cipherText.Length; i++)
+{
+    int ndx = (Array.IndexOf(alphabet, cipherText[i]) - keyNums[i % key.Length]);
+    while (ndx < 0) ndx += alphabet.Length;
+    plainText += alphabet[ndx];
+}
+Console.WriteLine(plainText);
